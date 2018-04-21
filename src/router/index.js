8f2a8bf26d06d749438434976f7349fc25eb03e3
EternalZZX <1301340187@qq.com>
2018-04-21 17:25:27 +0800

    route

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+import Home from '@/modules/home';
 
 Vue.use(Router);
 
 export default new Router({
-    routes: [
-        {
-            path: '/',
-            name: 'HelloWorld',
-            component: HelloWorld
-        }
-    ]
+    mode: 'history',
+    base: '/',
+    routes: [{
+        path: '/',
+        name: 'Home',
+        component: Home
+    }]
 });
