c1d1f21bb9b4f7f3692e3b6d2fdb14297b97d830
EternalZZX <1301340187@qq.com>
2018-05-01 11:23:23 +0800

    iconfont

--- src/modules/header.vue
+++ src/modules/header.vue
@@ -2,6 +2,7 @@
     <header class="et-header">
         <div class="et-header__inner">
             <img class="et-logo" src="images/et-logo-text.png">
+            <i class="et-icon ei-home-fill"></i>
         </div>
     </header>
 </template>
