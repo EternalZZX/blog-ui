9d5b4ecd048253baff5c379416e1e913a3302a7d
EternalZZX <1301340187@qq.com>
2018-05-16 13:55:25 +0800

    section

--- src/modules/section/section.vue
+++ src/modules/section/section.vue
@@ -1,30 +1,67 @@
 <template>
     <div class="et-layout">
         <nav class="et-nav">
-            <div class="et-nav__header">
-                <span class="et-nav__title">{{ $t("section.nav.title") }}</span>
-                <el-button class="et-nav__button" type="text">{{ $t("section.nav.create") }}</el-button>
-            </div>
-            <div class="et-nav__body">
-                <el-menu>
-                    <el-menu-item index="1">
-                        <span slot="title">{{ $t("section.nav.all") }}</span>
-                    </el-menu-item>
-                    <el-menu-item index="2">
-                        <span slot="title">{{ $t("section.nav.hot") }}</span>
-                    </el-menu-item>
-                    <el-menu-item index="3">
-                        <span slot="title">{{ $t("section.nav.follow") }}</span>
-                    </el-menu-item>
-                    <el-menu-item index="4">
-                        <span slot="title">{{ $t("section.nav.manage") }}</span>
-                    </el-menu-item>
-                </el-menu>
+            <div class="et-nav__wrapper">
+                <div class="et-nav__header">
+                    <span class="et-nav__title">{{ $t("section.nav.title") }}</span>
+                    <el-button class="et-nav__button" type="text">{{ $t("section.nav.create") }}</el-button>
+                </div>
+                <div class="et-nav__body">
+                    <el-menu>
+                        <el-menu-item index="1">
+                            <span slot="title">{{ $t("section.nav.all") }}</span>
+                        </el-menu-item>
+                        <el-menu-item index="2">
+                            <span slot="title">{{ $t("section.nav.hot") }}</span>
+                        </el-menu-item>
+                        <el-menu-item index="3">
+                            <span slot="title">{{ $t("section.nav.follow") }}</span>
+                        </el-menu-item>
+                        <el-menu-item index="4">
+                            <span slot="title">{{ $t("section.nav.manage") }}</span>
+                        </el-menu-item>
+                    </el-menu>
+                </div>
             </div>
         </nav>
         <div class="et-content">
             <div class="et-content__wrapper">
-                wrapper
+                <div class="et-card">
+                    <div class="et-card__cover"></div>
+                    <div class="et-card__body">
+                        <div class="et-card__body_left">
+                            <a class="et-card__title">Section Title</a>
+                            <span class="et-card__description">section description section description</span>
+                        </div>
+                        <div class="et-card__body_right">
+                            <span>content</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="et-card">
+                    <div class="et-card__cover"></div>
+                    <div class="et-card__body">
+                        <div class="et-card__body_left">
+                            <a class="et-card__title">Section Title</a>
+                            <span class="et-card__description">section description section description</span>
+                        </div>
+                        <div class="et-card__body_right">
+                            <span>content</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="et-card">
+                    <div class="et-card__cover"></div>
+                    <div class="et-card__body">
+                        <div class="et-card__body_left">
+                            <a class="et-card__title">Section Title</a>
+                            <span class="et-card__description">section description section description</span>
+                        </div>
+                        <div class="et-card__body_right">
+                            <span>content</span>
+                        </div>
+                    </div>
+                </div>
             </div>
             <div class="et-toolbar">
                 <div class="et-toolbar__header">
