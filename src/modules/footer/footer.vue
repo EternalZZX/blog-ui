28a730e3b9a7ccb8f9c9d830dc8b7c89f6d8946f
EternalZZX <1301340187@qq.com>
2020-02-26 10:21:01 +0800

    footer icp

--- src/modules/footer/footer.vue
+++ src/modules/footer/footer.vue
@@ -1,5 +1,7 @@
 <template>
-    <footer class="et-footer"></footer>
+    <footer class="et-footer">
+        <p>2020 © Powered by EternalZZX <a target="_blank" href="http://www.beian.miit.gov.cn">京ICP备17009377号</a></p>
+    </footer>
 </template>
 
 <script>
